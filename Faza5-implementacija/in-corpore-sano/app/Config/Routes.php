e8d3d3078f0982c30153fc62d7626997b4c1db32
miav123 <63118645+miav123@users.noreply.github.com>
2022-05-05 22:00:04 +0200

    Added routes for login and registration

--- Faza5-implementacija/in-corpore-sano/app/Config/Routes.php
+++ Faza5-implementacija/in-corpore-sano/app/Config/Routes.php
@@ -17,7 +17,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
+$routes->setDefaultController('Usercontroller');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
@@ -31,7 +31,9 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Usercontroller::index');
+$routes->match(['post', 'get'],'registerFirstPage', 'Usercontroller::registerFirstPage');
+$routes->match(['post', 'get'],'registerSecondPage', 'Usercontroller::registerSecondPage');
 
 /*
  * --------------------------------------------------------------------
