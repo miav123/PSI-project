a4e0c1d34e657e6c62245d967784030efff7d6af
miav123 <63118645+miav123@users.noreply.github.com>
2022-05-05 22:44:25 +0200

    Added header and footer for admin page

---
+++ Faza5-implementacija/in-corpore-sano/app/Views/templates/admin/footer.php
@@ -0,0 +1,6 @@
+<div id = "footer">
+    &copy; Copyright Team 7 2022
+</div>
+
+</body>
+</html>
---
+++ Faza5-implementacija/in-corpore-sano/app/Views/templates/admin/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="ICON" href="<?= base_url() ?>/images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="<?= base_url() ?>/assets/css/admin/styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
+            integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc"
+            crossorigin="anonymous"></script>
+
+    <title>Admin</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <a href="#"><img class="logo" src="<?= base_url() ?>/images/logo.png"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav" style="float:right;">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">CHALLENGES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">TRAINERS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">USERS</a>
+            </li>
+        </ul>
+    </div>
+</nav>
