81b4deab3ecf6cbda4fae4cbda905d9ebc185022
miav123 <vumi355@gmail.com>
2022-05-22 19:28:22 +0200

    Some changes to charts controller, not tested yet

--- Faza5-implementacija/in-corpore-sano/app/Controllers/User/Chartscontroller.php
+++ Faza5-implementacija/in-corpore-sano/app/Controllers/User/Chartscontroller.php
@@ -66,6 +66,30 @@ class Chartscontroller extends BaseController
         return $model->getLastYearDataForUser(2);
     }
 
+    private function recordForCurrentDay($month_data_sql, $day)
+    {
+        $r = null;
+        foreach ($month_data_sql as $record) {
+            if ($record['day_in_month'] == $day) {
+                $r = $record;
+                break;
+            }
+        }
+        return $r;
+    }
+
+    private function recordForCurrentDayInWeek($week_data_sql, $day)
+    {
+        $r = null;
+        foreach ($week_data_sql as $record) {
+            if ($record['day_in_week'] == $day) {
+                $r = $record;
+                break;
+            }
+        }
+        return $r;
+    }
+
     public function chart($type)
     {
 
@@ -86,47 +110,65 @@ class Chartscontroller extends BaseController
             1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April', 5 => 'May', 6 => 'June', 7 => 'July', 8 => 'August', 9 => 'September', 10 => 'October', 11 => 'November', 12 => 'December'
         ];
 
-        if(count($week_data_sql) < 7) {
-            for($i = 0; $i < 7 -count($week_data_sql); $i++) {
-                $week_data['label'][] = 'No data';
+        $months_days = [
+            'January' => 31, 'February' => 28, 'March' => 31, 'April' => 30, 'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31, 'September' => 30, 'October' => 31, 'November' => 30, 'December' => 31
+        ];
+
+//        foreach ($week_data_sql as $record) {
+//            $week_data['label'][] = $days_of_week[$record['day_of_week']];
+//            $week_data['data'][] = $record['result'];
+//        }
+
+        /*
+         *  WEEK DATA
+         */
+
+        for ($i = 1; $i <= 7; $i++) {
+            $record = $this->recordForCurrentDayInWeek($week_data_sql, $i);
+            $week_data['label'][] = $days_of_week[$i];
+            if ($record == null) {
                 $week_data['data'][] = 0;
+            } else {
+                $week_data['data'][] = $record['result'];
             }
         }
 
-        foreach ($week_data_sql as $record) {
-            $week_data['label'][] = $days_of_week[$record['day_of_week']];
-            $week_data['data'][] = $record['result'];
-        }
+        /*
+         *  MONTH DATA
+         */
 
-        if(count($month_data_sql) < 30) {
-            for($i = 0; $i < 30 -count($month_data_sql); $i++) {
-                $month_data['label'][] = 'No data';
+        $curr_month = intval(date('m', time()));
+        $num_of_days_in_month = $months_days[$months[$curr_month]];
+
+        for ($i = 1; $i <= $num_of_days_in_month; $i++) {
+            $record = $this->recordForCurrentDay($month_data_sql, $i);
+            if ($record == null) {
+                $month_data['label'][] = $i . "." . $curr_month . ".";
                 $month_data['data'][] = 0;
+            } else {
+                if ($record['month'] == $curr_month) {
+                    $month_data['label'][] = $i . "." . $curr_month . ".";
+                    $month_data['data'][] = $record['result'];
+                }
             }
         }
 
-        foreach ($month_data_sql as $record) {
-            $month_data['label'][] = $record['day'].".".$record['month'].".";
-            $month_data['data'][] = $record['result'];
-        }
-
-        if(count($year_data_sql) < 30) {
-            for($i = 0; $i < 30 -count($year_data_sql); $i++) {
-                $year_data['label'][] = 'No data';
-                $year_data['data'][] = 0;
-            }
-        }
+        /*
+         *  YEAR DATA
+         */
 
         foreach ($year_data_sql as $record) {
             $year_data['label'][] = $months[$record['month']];
-            $year_data['data'][] = $record['average'];
+            if ($record['result'] == null)
+                $year_data['data'][] = 0;
+            else
+                $year_data['data'][] = $record['result'];
+
         }
 
         $data['chart_week_data'] = json_encode($week_data);
 
-
         $data['chart_month_data'] = json_encode($month_data);
-
 
         $data['chart_year_data'] = json_encode($year_data);
 
