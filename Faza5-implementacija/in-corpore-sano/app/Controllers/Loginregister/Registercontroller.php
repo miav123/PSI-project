9bfc4abb7cccd69a7c8fbfb4b66fdb0da3904d8f
miav123 <vumi355@gmail.com>
2022-05-30 11:47:35 +0200

    Added doc comments and corrected small errors

--- Faza5-implementacija/in-corpore-sano/app/Controllers/Loginregister/Registercontroller.php
+++ Faza5-implementacija/in-corpore-sano/app/Controllers/Loginregister/Registercontroller.php
@@ -1,13 +1,27 @@
 <?php
 
+/*
+ *  Mia Vucinic
+ */
+
 namespace App\Controllers\Loginregister;
 
 use App\Controllers\BaseController;
 use App\Models\KorisnikModel;
 use App\Models\RegistrovaniKorisnikModel;
 
+
+/**
+ * Registercontroller - controller class that is used for registering new users
+ * @version 1.0
+ */
 class Registercontroller extends BaseController
 {
+    /**
+     * Function that is used on first page of register form. If all fields are valid, user will be redirected to next page.
+     *
+     * @return \CodeIgniter\HTTP\RedirectResponse|void
+     */
     public function register() {
 
         $data = [];
@@ -18,7 +32,7 @@ class Registercontroller extends BaseController
             $rules = [
                 'username' => 'required|min_length[1]|max_length[50]|is_unique[korisnik.kor_ime]',
                 'email' => 'required|min_length[6]|max_length[50]|valid_email|is_unique[korisnik.email]',
-                'password' => 'required|min_length[8]|max_length[255]',
+                'password' => 'required|min_length[8]|max_length[50]',
                 'password_repeat' => 'matches[password]',
             ];
 
@@ -38,15 +52,14 @@ class Registercontroller extends BaseController
                 $session->set('userdata', $user);
 
                 return redirect()->to('/registercontinue');
-
             }
-
         }
-
         echo view("login-registration-forms/register.php", $data);
     }
 
     /**
+     * Function that is used on second page of register form. If registration is successful, user will be redirected to log in screen and appropriate message will be shown.
+     *
      * @throws \ReflectionException
      */
     public function registercontinue() {
@@ -82,9 +95,11 @@ class Registercontroller extends BaseController
 
                 ]);
 
+                $id_kor = $modelUser->where('kor_ime', $userreg['username'])->first()['id_kor'];
+
                 $modelRegUser->save([
 
-                    'id_kor' => $modelUser->where('kor_ime', $userreg['username'])->first()['id_kor'],
+                    'id_kor' => $id_kor,
                     'visina' => $this->request->getVar('height'),
                     'tezina' => $this->request->getVar('weight'),
                     'br_tren' => $this->request->getVar('hours'),
@@ -98,11 +113,8 @@ class Registercontroller extends BaseController
                 $session->setFlashdata('success', 'Successful registration! You can log in now');
 
                 return redirect()->to('/');
-
             }
-
         }
-
         echo view("login-registration-forms/registerContinue.php", $data);
     }
 
