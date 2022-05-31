b7a2b80467463d7c12e719a45696cb65cc5279cd
miav123 <vumi355@gmail.com>
2022-05-31 22:42:58 +0200

    Refactored code and added new version of models

--- Faza5-implementacija/in-corpore-sano/app/Controllers/User/Chartscontroller.php
+++ Faza5-implementacija/in-corpore-sano/app/Controllers/User/Chartscontroller.php
@@ -7,9 +7,9 @@
 namespace App\Controllers\User;
 
 use App\Controllers\BaseController;
-use App\Models\UnosHraneModel_charts;
-use App\Models\UnosTreningaModel_charts;
-use App\Models\UnosVodeModel_charts;
+use App\Models\UnosHraneModel_charts_v1;
+use App\Models\UnosTreningaModel_charts_v1;
+use App\Models\UnosVodeModel_charts_v1;
 
 
 /**
@@ -19,22 +19,22 @@ use App\Models\UnosVodeModel_charts;
 class Chartscontroller extends BaseController
 {
     /**
-     * Function that returns a model corresponding to the type.
+     * Function that returns a model corresponding to the type passed as a parameter.
      * @param $type
-     * @return UnosHraneModel_charts|UnosTreningaModel_charts|UnosVodeModel_charts|null
+     * @return UnosHraneModel_charts_v1|UnosTreningaModel_charts_v1|UnosVodeModel_charts_v1|null
      */
     private function getModel($type) {
         $model = null;
         $db = db_connect();
         switch ($type) {
             case "food":
-                $model = new UnosHraneModel_charts($db);
+                $model = new UnosHraneModel_charts_v1($db);
                 break;
             case "water":
-                $model = new UnosVodeModel_charts($db);
+                $model = new UnosVodeModel_charts_v1($db);
                 break;
             case "training":
-                $model = new UnosTreningaModel_charts($db);
+                $model = new UnosTreningaModel_charts_v1($db);
                 break;
         }
         return $model;
@@ -74,42 +74,6 @@ class Chartscontroller extends BaseController
     }
 
     /**
-     * Function that checks if there is record in the database for a day passed as a parameter.
-     * @param $month_data_sql
-     * @param $day
-     * @return null
-     */
-    private function recordForCurrentDay($month_data_sql, $day)
-    {
-        $r = null;
-        foreach ($month_data_sql as $record) {
-            if ($record['day_in_month'] == $day) {
-                $r = $record;
-                break;
-            }
-        }
-        return $r;
-    }
-
-    /**
-     * Function that checks if there is record in the database for a day in a week passed as a parameter.
-     * @param $week_data_sql
-     * @param $day
-     * @return null
-     */
-    private function recordForCurrentDayInWeek($week_data_sql, $day)
-    {
-        $r = null;
-        foreach ($week_data_sql as $record) {
-            if ($record['day_in_week'] == $day) {
-                $r = $record;
-                break;
-            }
-        }
-        return $r;
-    }
-
-    /**
      * Function that collects chart data for given type, e.g. water, food and training, and passes it to the chart view page for plotting.
      * @param $type
      * @return void
@@ -136,60 +100,28 @@ class Chartscontroller extends BaseController
             1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April', 5 => 'May', 6 => 'June', 7 => 'July', 8 => 'August', 9 => 'September', 10 => 'October', 11 => 'November', 12 => 'December'
         ];
 
-        $months_days = [
-            'January' => 31, 'February' => 28, 'March' => 31, 'April' => 30, 'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31, 'September' => 30, 'October' => 31, 'November' => 30, 'December' => 31
-        ];
-
-//        foreach ($week_data_sql as $record) {
-//            $week_data['label'][] = $days_of_week[$record['day_of_week']];
-//            $week_data['data'][] = $record['result'];
-//        }
-
         /*
          *  WEEK DATA
          */
-
-        for ($i = 1; $i <= 7; $i++) {
-            $record = $this->recordForCurrentDayInWeek($week_data_sql, $i);
-            $week_data['label'][] = $days_of_week[$i];
-            if ($record == null) {
-                $week_data['data'][] = 0;
-            } else {
-                $week_data['data'][] = $record['result'];
-            }
+        foreach ($week_data_sql as $record) {
+            $week_data['label'][] = $days_of_week[$record['day_of_week']];
+            $week_data['data'][] = $record['result'];
         }
 
         /*
          *  MONTH DATA
          */
-
-        $curr_month = intval(date('m', time()));
-        $num_of_days_in_month = $months_days[$months[$curr_month]];
-
-        for ($i = 1; $i <= $num_of_days_in_month; $i++) {
-            $record = $this->recordForCurrentDay($month_data_sql, $i);
-            if ($record == null) {
-                $month_data['label'][] = $i . "." . $curr_month . ".";
-                $month_data['data'][] = 0;
-            } else {
-                if ($record['month'] == $curr_month) {
-                    $month_data['label'][] = $i . "." . $curr_month . ".";
-                    $month_data['data'][] = $record['result'];
-                }
-            }
+        foreach ($month_data_sql as $record) {
+            $month_data['label'][] = $record['day_in_month'] . "." . $record['month'] . ".";
+            $month_data['data'][] = $record['result'];
         }
 
         /*
          *  YEAR DATA
          */
-
         foreach ($year_data_sql as $record) {
             $year_data['label'][] = $months[$record['month']];
-            if ($record['result'] == null)
-                $year_data['data'][] = 0;
-            else
-                $year_data['data'][] = $record['result'];
-
+            $year_data['data'][] = $record['result'];
         }
 
         $data['chart_week_data'] = json_encode($week_data);
