73184c1b97b130f71390ca0425afd9481c850184
miav123 <vumi355@gmail.com>
2022-06-02 21:05:44 +0200

    Added new field for profile picture url

--- Faza5-implementacija/in-corpore-sano/app/Models/RegistrovaniKorisnikModel.php
+++ Faza5-implementacija/in-corpore-sano/app/Models/RegistrovaniKorisnikModel.php
@@ -5,7 +5,7 @@ use CodeIgniter\Model;
 class RegistrovaniKorisnikModel extends Model
 {
     protected $table      = 'registrovani_korisnik';
-    protected $allowedFields = ['id_kor', 'visina', 'tezina', 'br_tren', 'bodovi'];
+    protected $allowedFields = ['id_kor', 'visina', 'tezina', 'br_tren', 'bodovi', 'url_profilne_slike'];
     protected $primaryKey = 'id_kor';
 
 }
