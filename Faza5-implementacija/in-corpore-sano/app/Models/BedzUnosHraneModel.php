1919567af5058bba7aade79e120559caac1218ae
miav123 <vumi355@gmail.com>
2022-06-10 10:25:24 +0200

    Updated project assignment

--- Faza5-implementacija/in-corpore-sano/app/Models/BedzUnosHraneModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Models;
-
-use CodeIgniter\Model;
-class BedzUnosHraneModel extends Model{
-    protected $table      = 'bedz_unos_hrane';
-    protected $allowedFields = ['id_bedz', 'kolicina_kcal'];
-    protected $primaryKey = 'id_bedz';
-}
--- Faza5-implementacija/in-corpore-sano/app/Models/BedzUnosTreningModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-use CodeIgniter\Model;
-class BedzUnosTreningModel extends Model {
-    protected $table      = 'bedz_unos_trening';
-    protected $allowedFields = ['id_bedz', 'idTipTren','vreme_tren'];
-    protected $primaryKey = 'id_bedz';
-}
